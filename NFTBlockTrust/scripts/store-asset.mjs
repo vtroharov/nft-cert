920da9ac8fa4284949da635415230ba8077021d2
MBiallas <mbiallas@palo-it.com>
2022-05-03 21:31:59 +1000

    add video upload

--- NFTBlockTrust/scripts/store-asset.mjs
+++ NFTBlockTrust/scripts/store-asset.mjs
@@ -7,10 +7,14 @@ const API_KEY = process.env.NFT_STORAGE_API_KEY
 
 async function storeAsset() {
    const client = new NFTStorage({ token: API_KEY })
-   const metadata = await client.store({
+   const metadata = await client.store({ 
        name: 'abc',
-       description: 'xxx',
-       animation_url: 'ipfs://',
+       description: 'abc1',
+       animation_url: new File(
+        [await fs.promises.readFile('assets/NFTVideo.mp4')],
+        'NFTVideo.mp4',
+        { type: 'video/mp4' }
+    ),
        image: new File(
            [await fs.promises.readFile('assets/Preview.png')],
            'Preview.png',
@@ -19,6 +23,13 @@ async function storeAsset() {
    })
    console.log("Metadata stored on Filecoin and IPFS with URL:", metadata.url)
 }
+
+storeAsset()
+   .then(() => process.exit(0))
+   .catch((error) => {
+       console.error(error);
+       process.exit(1);
+   });
 
 storeAsset()
    .then(() => process.exit(0))
