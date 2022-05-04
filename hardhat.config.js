6b4cc8b1666244e41c5e73dfd28a8c0230e7e655
Andrei Maslennikov <amaslennikov@palo-it.com>
2022-05-04 13:04:51 +1000

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,53 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+require('dotenv').config();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const {
+  ETHEREUM_API_URL,
+  ROPSTEN_API_URL,
+  RINKENBY_API_URL,
+  POLYGON_API_URL,
+  MUMBAI_API_KEY,
+  } = process.env;
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const {
+  ETHEREUM_PRIVATE_KEY,
+  ROPSTEN_PRIVATE_KEY,
+  RINKENBY_PRIVATE_KEY,
+  POLYGON_PRIVATE_KEY,
+  MUMBAI_PRIVATE_KEY
+} = process.env;
+
+module.exports = {
+  solidity: "0.8.6",
+
+  defaultNetwork: "ropsten",
+
+  networks: {
+    hardhat: {},
+    PolygonMumbai: {
+      url: MUMBAI_API_KEY,
+      accounts: [MUMBAI_PRIVATE_KEY]
+    },
+    Polygon: {
+      url: POLYGON_API_URL,
+      accounts: [POLYGON_PRIVATE_KEY]
+    },
+    rinkenby: {
+      url: RINKENBY_API_URL,
+      accounts: [`0x${RINKENBY_PRIVATE_KEY}`]
+    },
+    ropsten: {
+      url: ROPSTEN_API_URL,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: ETHEREUM_API_URL,
+      accounts: [`0x${ETHEREUM_PRIVATE_KEY}`]
+    }
+  },
+};
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.6",
-};
