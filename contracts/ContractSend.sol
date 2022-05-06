32963f017639b5ec56f18accb432628d1a42c3cd
MBiallas <mbiallas@palo-it.com>
2022-05-06 17:33:27 +1000

    Update ContractSend.sol
    added simple transforFrom function

--- contracts/ContractSend.sol
+++ contracts/ContractSend.sol
@@ -6,6 +6,20 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+
 contract NoBurnToken is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
@@ -15,7 +29,7 @@ contract NoBurnToken is ERC721URIStorage, Ownable {
   /**
      * @dev Mints `tokenId` and transfers it to `to`.
      *
-     * WARNING: Usage of this method is discouraged, use {_safeMint} whenever possible
+     * WARNING: Usage of _mint method is discouraged, use {_safeMint} whenever possible
      *
      * Requirements:
      *
@@ -30,4 +44,30 @@ contract NoBurnToken is ERC721URIStorage, Ownable {
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
+     /**
+     * @dev Safely transfers `tokenId` token from `from` to `to`.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must exist and be owned by `from`.
+     * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
+     *
+     * Emits a {Transfer} event.
+     */
+       function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override {
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            'ERC721: transfer caller is not owner nor approved'
+        );
+        _transfer(from, to, tokenId);
+    }
+
+    
 }
