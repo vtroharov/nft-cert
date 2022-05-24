0da5fe6e1b39adb54d3c7bc7043563899103cd21
Vladimir Troharov <vtroharov@gmail.com>
2022-05-24 13:09:48 +1000

    Blocktrust Marketplace Mint

--- scripts/verify-nfts.mjs
+++ scripts/verify-nfts.mjs
@@ -1,20 +1,19 @@
-import { createAlchemyWeb3 } from "@alch/alchemy-web3";
+import { createAlchemyWeb3 } from '@alch/alchemy-web3';
 import fs from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const NETWORK_API_URL = process.env.NETWORK_API_URL
-const MINT_TO_WALLET = process.env.MINT_TO_WALLET
-const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS
+const NETWORK_API_URL = process.env.NETWORK_API_URL;
+const MINT_TO_WALLET = process.env.MINT_TO_WALLET;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
 const web3 = createAlchemyWeb3(NETWORK_API_URL);
 
-const nfts = await web3.alchemy.getNfts({owner: MINT_TO_WALLET})
+const nfts = await web3.alchemy.getNfts({ owner: MINT_TO_WALLET });
 
 let rawdata = fs.readFileSync('./meta_uri_copy.json');
 let assets = JSON.parse(rawdata);
 
-let contract_total = 0;
 let minted = [];
 let in_contract = [];
 
@@ -23,35 +22,40 @@ function printContract() {
   for (let i = 0; i < nfts.totalCount; i++) {
     // Extract all the NFTs associated with the Destination address
     if (nfts.ownedNfts[i].contract.address === CONTRACT_ADDRESS.toLowerCase()) {
-      in_contract.push([nfts.ownedNfts[i].title, nfts.ownedNfts[i].tokenUri.raw])
-      contract_total++;
+      in_contract.push([
+        nfts.ownedNfts[i].title,
+        nfts.ownedNfts[i].tokenUri.raw,
+      ]);
     }
     // Compare all the NFTs minted with the list of NFTs stored on nft.storage
     for (let j = 0; j < assets.length; j++) {
       if (assets[j].meta === nfts.ownedNfts[i].tokenUri.raw) {
-        minted.push([nfts.ownedNfts[i].title, assets[j].meta])
+        minted.push([nfts.ownedNfts[i].title, assets[j].meta]);
       }
     }
   }
 }
 
 function printWallet() {
-  setTimeout(function() { 
-    console.log("NFTs in Contract:")
+  setTimeout(function () {
+    console.log('NFTs in Contract:');
     for (let i = 0; i < in_contract.length; i++) {
-      console.log("NFT:", in_contract[i][0], "-", in_contract[i][1]);
+      console.log('NFT:', in_contract[i][0], '-', in_contract[i][1]);
     }
-    console.log("==================================================================================================")
-    console.log("NFTs Minted in Wallet:");
+    console.log(
+      '=================================================================================================='
+    );
+    console.log('NFTs Minted in Wallet:');
     for (let i = 0; i < minted.length; i++) {
-      console.log("Successfully Minted:", minted[i][0], "-", minted[i][1])
+      console.log('Successfully Minted:', minted[i][0], '-', minted[i][1]);
     }
-    console.log("==================================================================================================")
-    console.log("Total NFTs in wallet:", nfts.totalCount);
-    console.log("Total NFTs in contract:", contract_total);
+    console.log(
+      '=================================================================================================='
+    );
+    console.log('Total NFTs in wallet:', nfts.totalCount);
+    console.log('Total NFTs in contract:', in_contract.length);
   }, 1000);
 }
 
-
-printContract()
-printWallet()
+printContract();
+printWallet();
